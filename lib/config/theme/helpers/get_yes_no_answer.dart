1d767faf807e50919c670482e657df6b48d22c65
Cristian <cristianariasu13@gmail.com>
2025-07-21 18:18:54 -0500

    Implementar modelo YesNoModel(maper) para mapear respuesta de la API

--- lib/config/theme/helpers/get_yes_no_answer.dart
+++ lib/config/theme/helpers/get_yes_no_answer.dart
@@ -1,17 +1,19 @@
 import 'package:dio/dio.dart';
 import 'package:yes_no_app/domain/entities/message.dart';
+import 'package:yes_no_app/infrastructure/models/yes_no_model.dart';
 
 class GetYesNoAnswer {
   final _dio = Dio();
 
   Future<Message> getAnswer() async {
     final respose = await _dio.get('https://yesno.wtf/api');
-    respose.data['answer'];
+
+    final yesNoModel = YesNoModel.fromJsonMap(respose.data);
 
     return Message(
-      text: respose.data['answer'] == 'yes' ? 'Si' : 'No',
+      text: yesNoModel.answer == 'yes' ? 'Si' : 'No',
       fromWho: FromWho.his,
-      imgUrl: respose.data['image'],
+      imgUrl: yesNoModel.image,
     );
   }
 }
